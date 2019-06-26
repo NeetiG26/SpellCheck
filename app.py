1f7477939c6f7d6a2dfa01744fbbbe96c02d224b
Neeti Goenka <neeti.goenka@quantiphi.com>
2019-06-26 22:51:12 +0530

    Spell Checker

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+from spell import *
+
+app = Flask(__name__)
+
+@app.route('/spellCorrect')
+def get_page():
+	return render_template('page.html')
+
+@app.route('/spellCorrect', methods=['POST'])
+def spellCorrect():
+	word = request.form['word']
+	candidate = candidates(word)
+	segments = segment(word)
+	return render_template('page.html', candidates = candidate, segments=segments )
+
+if __name__ == '__main__':
+   app.run(debug = True)
---
+++ spell.py
@@ -0,0 +1,96 @@
+"""
+Spelling Corrector in Python 3
+Ref: http://norvig.com/spell-correct.html
+"""
+
+
+import re
+from collections import Counter
+
+def words(text): 
+    return re.findall(r'\w+', text.lower())
+
+WORDS = Counter(words(open('big.txt').read()))
+
+def P(word, N=sum(WORDS.values())): 
+    "Probability of `word`."
+    return WORDS[word] / N
+
+def Pwords(words):
+    "Probability of words, assuming each word is independent of others."
+    return product(P(w) for w in words)
+
+def product(nums):
+    "Multiply the numbers together.  (Like `sum`, but with multiplication.)"
+    result = 1
+    for x in nums:
+        result *= x
+    return result
+
+def correction(word): 
+    "Most probable spelling correction for word."
+    return max(candidates(word), key=P)
+
+def candidates(word): 
+    "Generate possible spelling corrections for word."
+    word = word.lower()
+    word = re.sub(r"\W", "", word, flags=re.I) # To clear all non word characters from the text
+    return (sorted((known([word]) or known(edits1(word)) or known(edits2(word)) or [word]) , key = P ))
+
+def known(words): 
+    "The subset of `words` that appear in the dictionary of WORDS."
+    return set(w for w in words if w in WORDS)
+
+def edits1(word):
+    "All edits that are one edit away from `word`."
+    letters    = 'abcdefghijklmnopqrstuvwxyz'
+    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
+    deletes    = [L + R[1:]               for L, R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
+    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+    inserts    = [L + c + R               for L, R in splits for c in letters]
+    return set(deletes + transposes + replaces + inserts)
+
+def edits2(word): 
+    "All edits that are two edits away from `word`."
+    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+
+def memo(f):
+    "Memoize function f, whose args must all be hashable."
+    cache = {}
+    def fmemo(*args):
+        if args not in cache:
+            cache[args] = f(*args)
+        return cache[args]
+    fmemo.cache = cache
+    return fmemo
+
+
+
+def splits(text, start=0, L=20):
+    "Return a list of all (first, rest) pairs; start <= len(first) <= L."
+    return [(text[:i], text[i:]) 
+            for i in range(start, min(len(text), L)+1)]
+
+
+
+@memo
+def segment(text):
+    "Return a list of words that is the most probable segmentation of text."
+    if not text: 
+        return []
+    else:
+        text = text.lower()
+        text = re.sub(r"\W", "", text, flags=re.I) # To clear all non word characters from the text
+        candidates = ([first] + segment(rest) 
+                      for (first, rest) in splits(text, 1))
+        return max(candidates, key=Pwords)
+
+
+
+
+if __name__ == '__main__':
+    print(candidates("hume"))
+    print(segment('1998-splot-assignment'))
+    for ele in (segment('humeassignment')):
+        print(ele)
